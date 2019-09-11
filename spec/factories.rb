8fe7156bed46e5f90f31117985e94abcc0febacc
Jeremy Hall <jhall229@gmail.com>
2019-09-11 21:38:25 +0000

    Added gram detail page

---
+++ spec/factories.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    sequence :email do |n|
+       "dummyEmail#{n}@gmail.com" 
+    end
+    password { "secretPassword" }
+    password_confirmation { "secretPassword" }
+  end
+
+  factory :gram do
+    message { "Hello" }
+    association :user
+  end  
+end
